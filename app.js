4e5a51bc1fbe99c67db85e96562dc545a8fc5fe8
yiiu <yu.gintoki@gmail.com>
2017-03-21 16:49:40 +0800

    :fish: updated config

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ import router from './routes'
 import use from './server/use'
 import logger from './logger'
 import './server/mailer.js'
+import config from './config.json'
+
 const app = new express()
 
 use(app)
 
 router(app)
 
-app.listen(3000, function(){
-    console.log('App (dev) is now running on port 3000!')
+app.listen(config.port, function(){
+    console.log(`App (dev) is now running on port ${config.port}!`)
 })
